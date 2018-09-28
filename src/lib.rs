6836c49c9abb80a9301bcb0a7993da9c84aa351d
Techcable <Techcable@techcable.net>
2018-09-27 18:45:45 -0700

    Remove extern crate declarations because they confuse IntelliJ
    As of the 2018 edition they're now optional. I used to keep them in
    because IntelliJ couldn't live without them. Now I have to remove htem
    because IntelliJ can't handle it.
    
    To think this is the *best* rust IDE....

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,3 @@
-extern crate indexmap;
-extern crate difference;
-extern crate itertools;
-extern crate fnv;
-extern crate lazycell;
-extern crate owning_ref;
-extern crate parking_lot;
-
 pub mod types;
 pub mod descriptor;
 pub mod mappings;
